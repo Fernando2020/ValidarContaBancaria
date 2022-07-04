7ce34b133bd24a191d5958c220fdf80bc696f161
Fernando Nogueira <fgeraldo11@gmail.com>
2022-07-04 13:51:24 -0300

    [ADD] Adicionada o serviço de validar conta do banco Santander

--- src/ValidarContaBancaria.Api/Startup.cs
+++ src/ValidarContaBancaria.Api/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ValidarContaBancaria.Service;
 
 namespace ValidarContaBancaria.Api
 {
@@ -27,6 +22,9 @@ namespace ValidarContaBancaria.Api
         {
 
             services.AddControllers();
+
+            services.AddServices();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ValidarContaBancaria.Api", Version = "v1" });
---
+++ src/ValidarContaBancaria.Service/Services/SantanderService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ValidarContaBancaria.Core.Dtos;
+using ValidarContaBancaria.Service.Interfaces;
+
+namespace ValidarContaBancaria.Service.Services
+{
+    public class SantanderService : ISantanderService
+    {
+        public bool ValidarContaCorrente(ValidarContaCorrenteComAgenciaDto dto)
+        {
+            var produtos = new List<int>();
+            var pesos = new List<int> { 9, 7, 3, 1, 0, 0, 9, 7, 1, 3, 1, 9, 7, 3 };
+            var agencia = dto.Agencia.PadLeft(4, '0');
+            var dv = dto.ContaCorrente.Last().ToString();
+            var conta = dto.ContaCorrente.Replace("-", string.Empty);
+            conta = conta.Substring(0, conta.Length - 1).PadLeft(8, '0');
+
+            conta = agencia + "00" + conta;
+
+            for (int i = 0; i < pesos.Count; i++)
+            {
+                var valor = (pesos[i] * Convert.ToInt32(conta[i].ToString())).ToString();
+                produtos.Add(Convert.ToInt32(valor.Last().ToString()));
+            }
+
+            var soma = produtos.Sum().ToString();
+            var resultado = 10 - Convert.ToInt32(soma.Last().ToString());
+
+            return dv == resultado.ToString();
+        }
+    }
+}
